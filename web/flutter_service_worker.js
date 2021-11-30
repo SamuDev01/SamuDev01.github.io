79edb211b47467e62590c44d4c805cc0173658e4
SamuDev01 <93723789+SamuDev01@users.noreply.github.com>
2021-11-30 13:53:37 +0100

    Add files via upload

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -49,9 +49,9 @@ const RESOURCES = {
 "images/icon-256x256.png": "8b156e42791403eae39dc047bb41be42",
 "images/icon-384x384.png": "2e4e1aa47cf34048258d9a759bc5ea58",
 "images/icon-512x512.png": "010556405890e3ecb97642459d1732a9",
-"index.html": "25f3d39ba97dd7b8a98097fc928bd8a1",
-"/": "25f3d39ba97dd7b8a98097fc928bd8a1",
-"main.dart.js": "9cb2db434ffcc3512700b355f3e52d91",
+"index.html": "2acb7ce35434e9869fb86dd68f7e8115",
+"/": "2acb7ce35434e9869fb86dd68f7e8115",
+"main.dart.js": "f1ee62df71554ad79d3d9714c7b9f28d",
 "manifest.json": "39a16bfebc1d31ba1da170a94f25ca4d",
 "manifest.webmanifest": "e345aba35581386a92bbf473b7e647ee",
 "version.json": "b557acc2fac715de8d64feeef31c33bf"
